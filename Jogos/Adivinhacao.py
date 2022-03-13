e586938331302f4956aa8c44fa6a6c41ee10edd0
Marconi <marconifrancisco@live.com>
2022-03-12 23:49:20 -0300

    Usando variáveis com condições
    Variáveis com as condições são usadas para testar o número caso seja maior ou menor. A condição "elif" é usada pela primeira vez.

--- Jogos/Adivinhacao.py
+++ Jogos/Adivinhacao.py
@@ -8,14 +8,22 @@ chute_str = input("Digite o seu número: ")
 
 chute_int = int(chute_str)
 
+acertou = chute_int == numero_secreto
+maior   = chute_int > numero_secreto
+menor   = chute_int < numero_secreto
+
 print("Você digitou", chute_int)
 
-if(numero_secreto == chute_int):
+if(acertou):
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if(maior):
+        print("Você errou! Seu chute foi maior que o número secreto.")
+    elif(menor):
+        print("Você errou! Seu chute foi menor que o número secreto.")
 
 print("Fim do jogo!")
+
 
 
 
