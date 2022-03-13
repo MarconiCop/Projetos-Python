e6f4167aa352eef1e0a550777cd948dde680ad24
Marconi <marconifrancisco@live.com>
2022-03-13 11:35:51 -0300

    Primeiro uso de While e função .format()
    Usado o while para impor a xcondição de tentativas e a função .format para tornar o código mais agradável.

--- Jogos/Adivinhacao.py
+++ Jogos/Adivinhacao.py
@@ -3,24 +3,29 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
+while(rodada <= total_de_tentativas):
 
-chute_int = int(chute_str)
+    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
 
-acertou = chute_int == numero_secreto
-maior   = chute_int > numero_secreto
-menor   = chute_int < numero_secreto
+    chute_str = input("Digite o seu número: ")
+    chute_int = int(chute_str)
+    print("Você digitou", chute_int)
 
-print("Você digitou", chute_int)
+    acertou = chute_int == numero_secreto
+    maior   = chute_int > numero_secreto
+    menor   = chute_int < numero_secreto
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! Seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você errou! Seu chute foi menor que o número secreto.")
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o número secreto.")
+    rodada = rodada + 1
 
 print("Fim do jogo!")
 
