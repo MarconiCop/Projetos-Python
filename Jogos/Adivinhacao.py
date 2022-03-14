19022a0dcdaec1aa9b2cf6bdadea25b72d6e2085
Marconi <marconifrancisco@live.com>
2022-03-14 14:03:30 -0300

    Adicionados níveis e pontuação
    Adicionados as variáveis de níveis baseadas nos números de tentativas e a pontuação. Inclui o uso da função abs()

--- Jogos/Adivinhacao.py
+++ Jogos/Adivinhacao.py
@@ -6,34 +6,48 @@ print("*********************************")
 
 
 numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+pontos = 1000
 
-total_de_tentativas = 3
+print("Qual nível de dificuldade você deseja?")
+print("(1) Fácil (2) Médio (3) Difícil")
 
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
 
     print("Tentativa {:02d} de {:02d}".format(rodada, total_de_tentativas))
 
-    chute_str = input("Digite o seu número entre 1 e 100: ")
-    chute_int = int(chute_str)
-    print("Você digitou", chute_int)
+    chute = int(input("Digite o seu número entre 1 e 100: "))
+    print("Você digitou", chute)
 
-    if(chute_int < 1 or chute_int > 100):
+    if(chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
         continue
 
-    acertou = chute_int == numero_secreto
-    maior   = chute_int > numero_secreto
-    menor   = chute_int < numero_secreto
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! Seu chute foi maior que o número secreto.")
         elif(menor):
             print("Você errou! Seu chute foi menor que o número secreto.")
+
+        pontos_perdidos = abs(numero_secreto - chute)
+
+        pontos = pontos - pontos_perdidos
 
 print("O número era", numero_secreto)
 print("Fim do jogo!")
