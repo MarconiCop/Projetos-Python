e946e5b1e3b3e0aa2817e4f0a443d71437278396
Marconi <marconifrancisco@live.com>
2022-03-14 11:04:28 -0300

    Uso de for e formatação americana
    O código passa a declarar a variável rodada dentro de um loop for. Além disso, a formatação foi melhorada iterando de maneira diferente.

--- Jogos/Adivinhacao.py
+++ Jogos/Adivinhacao.py
@@ -2,17 +2,22 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
+
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
 
-    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+for rodada in range(1, total_de_tentativas + 1):
 
-    chute_str = input("Digite o seu número: ")
+    print("Tentativa {:02d} de {:02d}".format(rodada, total_de_tentativas))
+
+    chute_str = input("Digite o seu número entre 1 e 100: ")
     chute_int = int(chute_str)
     print("Você digitou", chute_int)
+
+    if(chute_int < 1 or chute_int > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute_int == numero_secreto
     maior   = chute_int > numero_secreto
@@ -20,12 +25,12 @@ while(rodada <= total_de_tentativas):
 
     if(acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! Seu chute foi maior que o número secreto.")
         elif(menor):
             print("Você errou! Seu chute foi menor que o número secreto.")
-    rodada = rodada + 1
 
 print("Fim do jogo!")
 
