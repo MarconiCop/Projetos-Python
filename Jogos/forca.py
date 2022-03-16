53555498392db00d097ec76326f41cc54fe12415
Marconi <marconifrancisco@live.com>
2022-03-16 15:00:34 -0300

    Definido o loop de verificação
    Foi definido um loop de verificação para cada letra da palavra, incluindo um chute que recebe a função strip() para retirar espaçamentos e um index que aumenta a cada vez do loop. Usado também a função upper() para comparar também com letras maiúsculas.

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -12,7 +12,16 @@ def jogar():
 
     #Enquanto não enforcou E não acertou
     while(not enforcou and not acertou):
-        print("jogando...")
+
+        chute = input("Digite sua letra: ")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {} ".format(letra, index))
+            index = index + 1
 
     print("Fim do jogo!")
 
