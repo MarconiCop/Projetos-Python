24c4e6f5d61fe31d5ac3d010aa81f12bef9615ce
Marconi <marconifrancisco@live.com>
2022-03-17 10:25:57 -0300

    Lista adicionada
    Usando uma lista para as letras acertadas na qual salva na posição caso o usuário acerte alguma letra.

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -6,9 +6,12 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+
+    print((letras_acertadas))
 
     #Enquanto não enforcou E não acertou
     while(not enforcou and not acertou):
@@ -19,9 +22,12 @@ def jogar():
         index = 0
 
         for letra in palavra_secreta:
+
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {} ".format(letra, index))
+                letras_acertadas[index] = chute
             index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo!")
 
