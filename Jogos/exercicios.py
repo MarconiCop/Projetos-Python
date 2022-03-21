16cca3106a5383b770892f2594f5231ce2ea6e8a
Marconi <marconifrancisco@live.com>
2022-03-21 12:31:27 -0300

    Maior número Primo
    Exercício feito para calcular o maior número primo. Roda infinitamente pelo número ser muito grande.

--- Jogos/exercicios.py
+++ Jogos/exercicios.py
@@ -1,4 +1,5 @@
-#Exercício_1
+#####################################################################Exercício_1
+
 def multiplos():
 
     soma = 0
@@ -11,5 +12,30 @@ def multiplos():
 
     print("A soma dos múltiplos de 3 e 5 menores que 1000 são {}".format(soma))
 
+
+######################################################################Exercício_3
+
+def fator():
+
+
+    lista_multiplos = []
+    for numero_multiplo in range(2, 600851475143):
+
+        if(13195 % numero_multiplo == 0):
+
+            lista_multiplos.append(numero_multiplo)
+
+            for numero_primo in range(2, numero_multiplo):
+
+                if(numero_multiplo % numero_primo == 0):
+
+                    if(numero_multiplo in lista_multiplos):
+
+                        lista_multiplos.remove(numero_multiplo)
+
+
+    print(lista_multiplos[len(lista_multiplos)-1])
+
 if(__name__ == "__main__"):
-    multiplos()
+    fator()
+#################################################################################
