fac542067c1611b58904a5d17ddb034fc3189163
Marconi <marconifrancisco@live.com>
2022-03-24 17:04:54 -0300

    Exercícios feitos ( 11 ao 15)
    Os exercícios de número 11 ao 15 sobre estrutura sequencial foram feitos

--- Exercicios/estrutura_sequencial.py
+++ Exercicios/estrutura_sequencial.py
@@ -71,5 +71,89 @@ def temperatura_f():
     temperatura_f =  ((temperatura_c * 9) / 5) + 32
     print("A temperatura {}ªC equivale a {}ªF".format(temperatura_c, temperatura_f))
 
+#11. Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+#o produto do dobro do primeiro com metade do segundo .
+#a soma do triplo do primeiro com o terceiro.
+#o terceiro elevado ao cubo.
+
+def numero_onze():
+    numero_int_um = int(input("Digite seu primeiro número inteiro: "))
+    numero_int_dois = int(input("Digite seu segundo número inteiro: "))
+    numero_real = float(input("Digite seu número real: "))
+
+    produto = (2 * numero_int_um) * (numero_int_dois / 2)
+    soma =(3 * numero_int_um) + numero_real
+    elevado = numero_real * numero_real * numero_real
+
+    print(produto, soma, elevado)
+
+#12. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+
+def peso():
+    altura = float(input("Digite sua altura: "))
+    peso = round((72.7 * altura) - 58, 2)
+    print("Seu peso é {}!".format(peso))
+
+#13.  como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+#Para homens: (72.7*h) - 58
+#Para mulheres: (62.1*h) - 44.7
+
+def peso_ideal():
+    sexo = int(input("Digite 1 se você for homem ou digite 2 se você for mulher: "))
+    altura = float(input("Digite sua altura: "))
+    if(sexo == 1):
+        peso_homem = round((72.7 * altura) - 58, 2)
+        print("Seu peso ideal(homem) é {}!".format(peso_homem))
+    elif(sexo == 2):
+        peso_mulher = round((62.1 * altura) - 44.7, 2)
+        print("Seu peso ideal(mulher) é {}!".format(peso_mulher))
+
+#14. João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho.
+# Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente.
+# João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso.
+# Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar.
+# Imprima os dados do programa com as mensagens adequadas.
+
+def peixe():
+    peso = float(input("Digite o peso da quantidade pescada: "))
+
+    if(peso > 50):
+        excesso = round(peso - 50, 2)
+        multa = round(4 * excesso, 2)
+        print("João coletou {} kilos de peixe ultrapassando {} kilos e deve pagar {} reais de multa!".format(peso, excesso, multa))
+
+    else:
+        print("João coletou {} kilos de peixes e não deve multa".format(peso))
+
+#15. Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês,
+#sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+#salário bruto.
+#quanto pagou ao INSS.
+#quanto pagou ao sindicato.
+#o salário líquido.
+#calcule os descontos e o salário líquido, conforme a tabela abaixo:
+#+ Salário Bruto : R$
+#- IR (11%) : R$
+#- INSS (8%) : R$
+#- Sindicato ( 5%) : R$
+#= Salário Liquido : R$
+
+def salario_tipo():
+
+    dinheiro_hora = float(input("Quanto você ganha por hora? "))
+    numero_horas = float(input("Quantas horas você trabalha no mês? "))
+
+    salario_bruto = dinheiro_hora * numero_horas
+    salario_ir = (11 / 100) * salario_bruto
+    salario_inss = (8 / 100) * salario_bruto
+    salario_sindicato = (5 / 100) * salario_bruto
+    salario_liquido = salario_bruto - salario_ir - salario_inss - salario_sindicato
+
+    print("+ Salário Bruto : R$ {:.2f}".format(salario_bruto))
+    print("- IR (11%) : R$ {:.2f}".format(salario_ir))
+    print("- INSS (8%) : R$ {:.2f}".format(salario_inss))
+    print("- Sindicato ( 5%) : R$ {:.2f}".format(salario_sindicato))
+    print("= Salário Liquido : R$ {:.2f}".format(salario_liquido))
+
 if(__name__ == "__main__"):
-    temperatura_f()
+    salario_tipo()
