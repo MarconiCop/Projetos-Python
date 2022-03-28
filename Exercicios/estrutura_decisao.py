449e0959bf5c2f16991fe32faa629ce068c3c1ec
Marconi <marconifrancisco@live.com>
2022-03-27 21:19:35 -0300

    Exercício 19 feito
    Exercício 19 de estruturas de decisão feito

--- Exercicios/estrutura_decisao.py
+++ Exercicios/estrutura_decisao.py
@@ -190,5 +190,28 @@ def reajuste():
     print("O reajuste foi de: R${:.2f}".format(valor_do_aumento))
     print("Salário após reajuste: R${:.2f}".format(salario_com_reajuste))
 
+#19.Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, dezenas e unidades do mesmo.
+#Observando os termos no plural a colocação do "e", da vírgula entre outros. Exemplo:
+#326 = 3 centenas, 2 dezenas e 6 unidades
+#12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
+
+def casas_numero():
+    numero = int(input("Digite o número(1 a 1000): "))
+
+    if(numero < 10):
+        print("{} = {} unidade(s).".format(numero, numero))
+
+    if(numero >= 10 and numero < 100):
+        dezena = int(numero / 10)
+        unidade = int(numero - 10 * dezena)
+        print("{} = {} dezena(s) e {} unidade(s).".format(numero, dezena, unidade))
+
+    if(numero >= 100 and numero < 1000):
+        centena = int(numero/100)
+        dezena = int((numero % 100) / 10)
+        unidade = numero % 10
+
+        print("{} = {} centena(s), {} dezena(s) e {} unidade(s).".format(numero, centena, dezena, unidade))
+
 if(__name__ == "__main__"):
-    reajuste()
+    casas_numero()
