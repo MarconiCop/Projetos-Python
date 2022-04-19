086ddfc1d3bf48bbc67d190a4fcbeb1a449a27fb
Marconi <marconifrancisco@live.com>
2022-04-19 13:41:15 -0300

    Exercício 16 feito
    Exercício 16 de listas feito.

--- Exercicios/estrutura_listas.py
+++ Exercicios/estrutura_listas.py
@@ -377,5 +377,34 @@ def exibir_lista_notas():
     print("-------------------------")
 
 
+# 16. Utilize uma lista para resolver o problema a seguir. Uma empresa paga os seus vendedores com base em comissões. O
+# vendedor recebe $200 por semana mais 9 por cento das suas vendas brutas daquela semana. Por exemplo, um vendedor que
+# teve vendas brutas de $3000 numa semana recebe $200 mais 9 por cento de $3000, ou seja, um total de $470. Escreva
+# um programa (usando um array de contadores) que determine quantos vendedores receberam salários nos seguintes 
+# intervalos de valores: $200 - $299 $300 - $399 $400 - $499 $500 - $599 $600 - $699 $700 - $799 $800 - $899 $900 - 
+# $999 $1000 em diante Desafio: Crie uma fórmula para chegar na posição da lista a partir do salário, 
+# sem fazer vários ifs aninhados. 
+
+def exibir_lista_contador_salario():
+    contadores = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    salario = 0
+    salarios = []
+
+    while salario != -1:
+
+        salario = float(input("Digite o salário: "))
+
+        if salario != -1:
+            salario = salario + 200 + 0.09 * salario
+            salarios.append(salario)
+
+    for salario in salarios:
+        indice = int((salario / 100) - 2)
+        contadores[indice] += 1
+
+    print(salarios)
+    print(contadores)
+
+
 if __name__ == "__main__":
-    exibir_lista_notas()
+    exibir_lista_contador_salario()
