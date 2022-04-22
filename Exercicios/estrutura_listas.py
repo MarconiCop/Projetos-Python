661d0bead92f84608c2f182f0a1c3551190c649e
Marconi <marconifrancisco@live.com>
2022-04-22 20:05:12 -0300

    Exercício 20 feito
    Exercício 20 de listas feito.

--- Exercicios/estrutura_listas.py
+++ Exercicios/estrutura_listas.py
@@ -565,7 +565,6 @@ def calcula_percentual(numero_votos, total_votos):
 # vencedor da enquete.
 
 def exibir_lista_votacao_sistema():
-
     contadores = [0] * 6
     sistemas = (1, 2, 3, 4, 5, 6)
     percentuais = []
@@ -669,5 +668,60 @@ def calcula_percentual(numero_votos, total_votos):
     return percentual
 
 
+# 20. As Organizações Tabajara resolveram dar um abono aos seus colaboradores em reconhecimento ao bom resultado
+# alcançado durante o ano que passou. Para isto contratou você para desenvolver a aplicação que servirá como uma
+# projeção de quanto será gasto com o pagamento deste abono. Após reuniões envolvendo a diretoria executiva,
+# a diretoria financeira e os representantes do sindicato laboral, chegou-se a seguinte forma de cálculo: a.Cada
+# funcionário receberá o equivalente a 20% do seu salário bruto de dezembro; a.O piso do abono será de 100 reais,
+# isto é, aqueles funcionários cujo salário for muito baixo, recebem este valor mínimo; Neste momento, não se deve
+# ter nenhuma preocupação com colaboradores com tempo menor de casa, descontos, impostos ou outras particularidades.
+# O seu programa deverá permitir a digitação do salário de um número indefinido (desconhecido) de salários. Um valor de
+# salário igual a 0 (zero) encerra a digitação. Após a entrada de todos os dados o programa deverá calcular o valor
+# do abono concedido a cada colaborador, conforme a regra definida acima. Ao final, o programa deverá
+# apresentar: O salário de cada funcionário, com o valor do abono; O número total de funcionário
+# processado; O valor total a ser gasto com o pagamento do abono; O número de funcionário que receberá o valor
+# mínimo de 100 reais; O maior valor pago como abono; A tela abaixo é um exemplo de execução do programa, apenas para
+# fins ilustrativos. Os valores podem mudar a cada execução do programa.
+
+def exibir_lista_salario():
+    salarios = []
+    abonos = []
+    contador = 0
+
+    salario = int(input("Digite qualquer coisa (Exceto 0) para iniciar o programa:"))
+
+    if salario == 0:
+        return 1
+
+    print("Projeção de Gastos com Abono")
+    print("============================")
+    print("")
+
+    while salario != 0:
+        salario = int(input("Digite o salário: "))
+        salarios.append(salario)
+
+    for posicao in range(0, len(salarios)):
+        if salarios[posicao] < 1000:
+            abonos.append(100)
+            contador += 1
+        else:
+            abonos.append(0.20 * salarios[posicao])
+
+    contador -= 1
+    salarios.pop()
+    abonos.pop()
+
+    print("Salário    - Abono ")
+    for posicao in range(0, len(salarios)):
+        print("R$ {:.2f} - R$ {:.2f}".format(salarios[posicao], abonos[posicao]))
+
+    print("")
+    print("Foram processados {} colaboradores".format(len(salarios)))
+    print("Total gasto com abonos: R${:.2f}".format(sum(abonos)))
+    print("Valor mínimo pago a {} colaboradores".format(contador))
+    print("Maior valor de abono pago: R${:.2f}".format(max(abonos)))
+
+
 if __name__ == "__main__":
-    exibir_lista_votacao_sistema()
+    exibir_lista_salario()
