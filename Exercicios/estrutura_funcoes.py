b8a037c4ee92d96af63a825cc2e41faf477e4a6f
Marconi <marconifrancisco@live.com>
2022-04-27 12:31:13 -0300

    Exercício 2 feito
    Exercício 2 de listas feito

--- Exercicios/estrutura_funcoes.py
+++ Exercicios/estrutura_funcoes.py
@@ -12,5 +12,32 @@ def funcao(numero):
         print("  ".join((str(number) * number)))
 
 
-n = int(input("Digite o número: "))
-funcao(n)
+def principal():
+    n = int(input("Digite o número: "))
+    funcao(n)
+
+
+# 2. Faça um programa para imprimir:
+#     1
+#     1   2
+#     1   2   3
+#     .....
+#     1   2   3   ...  n
+# para um n informado pelo utilizador. Use uma função que receba um valor n inteiro imprima até a n-ésima linha.
+
+def funcao_dois(n):
+    for linha in range(1, n + 1):
+        for coluna in range(1, linha + 1):
+            print(coluna, end="  ")
+        print('')
+
+
+
+def principal_dois():
+    n = int(input("Digite o número: "))
+    funcao_dois(n)
+
+
+if __name__ == "__main__":
+    principal_dois()
+
