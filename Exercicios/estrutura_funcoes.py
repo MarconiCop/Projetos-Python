983336da6ebbba8b3f46ee9d7c465f80e8fcd8c3
Marconi <marconifrancisco@live.com>
2022-05-03 11:21:56 -0300

    Exercício 11 feito
    Exercício 11 de funções feito

--- Exercicios/estrutura_funcoes.py
+++ Exercicios/estrutura_funcoes.py
@@ -1,3 +1,7 @@
+import random
+import re
+
+
 # 1. Faça um programa para imprimir:
 #     1
 #     2   2
@@ -5,7 +9,6 @@
 #     .....
 #     n   n   n   n   n   n ... N
 # para um n informado pelo utilizador. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
-import random
 
 
 def funcao(numero):
@@ -241,5 +244,87 @@ def principal_dez():
         lancar = int(input("Digite 1 para lançar o dado 1 e 2: "))
 
 
+# 11. Data com mês por extenso. Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma ‘string’ no
+# formato D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
+
+
+def data(dia, mes, ano):
+    calendario = {"Janeiro": 1,
+                  "Fevereiro": 2,
+                  "Março": 3,
+                  "Abril": 4,
+                  "Maio": 5,
+                  "Junho": 6,
+                  "Julho": 7,
+                  "Agosto": 8,
+                  "Setembro": 9,
+                  "Outubro": 10,
+                  "Novembro": 11,
+                  "Dezembro": 12}
+
+    for meses, valor in calendario.items():
+        if mes == valor:
+            mes_extenso = meses
+
+    return print("{} de {} de {}".format(dia, mes_extenso, ano))
+
+
+def e_bissexto(ano):
+    if regra_um(ano) or regra_dois(ano):
+        return True
+
+
+def regra_um(ano):
+    if ano % 4 == 0 and ano % 100 != 0:
+        return True
+
+
+def regra_dois(ano):
+    if ano % 4 == 0 and ano % 100 == 0 and ano % 400 == 0:
+        return True
+
+
+def principal_onze():
+    data_escolhida = input("Digite a data no formato dd/mm/aaaa: ")
+
+    numeros_data = [int(numero) for numero in re.findall(r'-?\d+\.?\d*', data_escolhida)]
+
+    dia = int(numeros_data[0])
+    mes = int(numeros_data[1])
+    ano = int(numeros_data[2])
+    tupla_meses_trinta_um_dias = (1, 3, 5, 7, 9, 10, 12)
+    tupla_meses_trinta_dias = (4, 6, 8, 11)
+
+    if mes in tupla_meses_trinta_um_dias:
+        if dia <= 31:
+            if 1000 <= ano <= 9999:
+                data(dia, mes, ano)
+            else:
+                return 0
+        else:
+            return 0
+
+    elif mes in tupla_meses_trinta_dias:
+        if dia <= 30:
+            if 1000 <= ano <= 9999:
+                data(dia, mes, ano)
+        else:
+            return 0
+
+    elif mes == 2:
+        if e_bissexto(ano):
+            if dia <= 29:
+                data(dia, mes, ano)
+            else:
+                return 0
+        else:
+            if dia <= 28:
+                data(dia, mes, ano)
+            else:
+                return 0
+    else:
+        return 0
+
+
 if __name__ == "__main__":
-    principal_dez()
+    principal_onze()
