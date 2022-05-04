a8f7af2afaa63e1702daf855970f32925479753c
Marconi <marconifrancisco@live.com>
2022-05-04 12:39:44 -0300

    Exercício 12 feito
    Exercício 12 de funções feito

--- Exercicios/estrutura_funcoes.py
+++ Exercicios/estrutura_funcoes.py
@@ -1,5 +1,6 @@
 import random
 import re
+from random import sample
 
 
 # 1. Faça um programa para imprimir:
@@ -326,5 +327,22 @@ def principal_onze():
         return 0
 
 
+# 12. Embaralha palavra. Construa uma função que receba uma ‘string’ como parâmetro e devolva outra ‘string’ com os
+# carateres embaralhados. Por exemplo: se função receber a palavra python, pode retornar npthyo, ophtyn ou qualquer
+# outra combinação possível, de forma aleatória. Padronize na sua função que todos os caracteres serão devolvidos em
+# caixa alta ou caixa baixa, independentemente de como foram digitados.
+
+def embaralha(nome):
+    a = sample(nome, len(nome))
+    d = ''.join(a)
+    print(a)
+    print(d.lower())
+
+
+def principal_doze():
+    nome = input('Digite algo: ')
+    embaralha(nome)
+
+
 if __name__ == "__main__":
-    principal_onze()
+    principal_doze()
