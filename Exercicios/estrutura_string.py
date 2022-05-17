ee8f14f8b18594a9029dd9245593d4f13dd0b180
Marconi <marconifrancisco@live.com>
2022-05-17 15:07:42 -0300

    Exercícios de Strings terminados
    Exercícios de estruturas de strings terminado.

--- Exercicios/estrutura_string.py
+++ Exercicios/estrutura_string.py
@@ -187,7 +187,7 @@ def palindromo():
         print('Não é palíndromo.')
 
 
-# 8. Verificação de CPF. Desenvolva um programa que solicite a digitação de um número de CPF no formato
+# 9. Verificação de CPF. Desenvolva um programa que solicite a digitação de um número de CPF no formato
 # xxx.xxx.xxx-xx e indique se é um número válido ou inválido através da validação dos dígitos verificadores e dos
 # caracteres de formatação.
 
@@ -209,5 +209,91 @@ def valida_cpf():
     cpf_obj
 
 
+# 10. Número por extenso. Escreva um programa que solicite ao utilizador a digitação de um número até 99 e imprima-o na
+# tela por extenso.
+
+def converter_em_texto(numeral):
+    dicionario_numerico = {
+        0: 'zero', 1: 'um', 2: 'dois', 3: 'três', 4: 'quatro', 5: 'cinco',
+        6: 'seis', 7: 'sete', 8: 'oito', 9: 'nove', 10: 'dez',
+        11: 'onze', 12: 'doze', 13: 'treze', 14: 'quatorze', 15: 'quinze',
+        16: 'dezesseis', 17: 'dezessete', 18: 'dezoito', 19: 'dezenove', 20: 'vinte',
+        30: 'trinta', 40: 'quarenta', 50: 'cinquenta', 60: 'sessenta', 70: 'setenta',
+        80: 'oitenta', 90: 'noventa',
+    }
+
+    if numeral > 99 or numeral < 0:
+        raise ValueError('O número deve estar entre 0 e 99 (inclusive)')
+
+    if numeral < 20 or numeral % 10 == 0:
+        return dicionario_numerico.get(numeral)
+
+    decimal = numeral // 10 * 10
+    unidade = numeral % 10
+    extenso = f'{dicionario_numerico.get(decimal)} e {dicionario_numerico.get(unidade)}'
+    return extenso
+
+
+def principal_dez():
+    print('Insira um número:')
+    numeral = int(input())
+    numero_por_extenso = converter_em_texto(numeral)
+    print(numero_por_extenso)
+
+
+# 11. Jogo de Forca. Desenvolva um jogo da forca. O programa terá uma lista de palavras lidas de um arquivo texto e
+# escolherá uma aleatoriamente. O jogador poderá errar 6 vezes antes de ser enforcado.
+
+def principal_onze():
+    print("Feito em outro arquivo!")
+
+
+# 12. Valida e corrige número de telefone. Faça um programa que leia um número de telefone, e corrija o número no
+# caso deste conter somente 7 dígitos, acrescentando o '3' na frente. O usuário pode informar o número com ou sem o
+# traço separador.
+
+
+def principal_doze():
+    numero = int(input('Telefone: '))
+    novoNumero = ''
+    if len(str(numero)) < 8:
+        diferenca = 8 - len(str(numero))
+        novoNumero = '3' * diferenca
+
+    numeroFormatado = novoNumero + str(numero)
+    numeroFormatado = numeroFormatado[0:4] + '-' + numeroFormatado[5:]
+
+    print('Telefone possui %d dígitos. Vou acrescentar o digito três na frente.' % len(str(numero)))
+    print('Telefone corrigido sem formatação: %d' % numero)
+    print('Telefone corrigido com formatação: %s' % numeroFormatado)
+
+
+# 13. Jogo da palavra embaralhada. Desenvolva um jogo em que o utilizador tenha que adivinhar uma palavra que será
+# mostrada com as letras embaralhadas. O programa terá uma lista de palavras lidas de um arquivo texto e escolherá
+# uma aleatoriamente. O jogador terá seis tentativas para adivinhar a palavra. Ao final a palavra deve ser mostrada
+# na tela, informando se o utilizador ganhou ou perdeu o jogo.
+
+def principal_treze():
+    print("Feito em outro arquivo!")
+
+
+# 14. Leet spek generator. Leet é uma forma de se escrever o alfabeto latino usando outros símbolos em lugar das
+# letras, como números, por exemplo. A própria palavra leet admite muitas variações, como l33t ou 1337. O uso do leet
+# reflete uma subcultura relacionada ao mundo dos jogos de computador e ‘internet’, sendo muito usada para confundir os
+# iniciantes e afirmar-se como parte de um grupo. Pesquise sobre as principais formas de traduzir as letras. Depois,
+# faça um programa que peça um texto e transforme-o para a grafia leet speak.
+
+
+from utilitybelt import change_charset
+
+
+def principal_quatorze():
+    origspace = "abcdefghijklmnopqrstuvwxyz"
+    keyspace = "4bcd3fgh1jklmn0pqr57uvwxyz"
+    palavra = input("Digite a palavra:")
+
+    print(change_charset("{}".format(palavra), origspace, keyspace))
+
+
 if __name__ == "__main__":
-    valida_cpf()
+    principal_doze()
